84f2cbcffcbeba350abd8ef06b191dcd7d153c83
chenkai <2081058113@qq.com>
2025-05-07 16:55:35 +0800

    finished lab3

--- src/arp.c
+++ src/arp.c
@@ -58,6 +58,20 @@ void arp_print() {
  */
 void arp_req(uint8_t *target_ip) {
     // TO-DO
+
+    //初始化缓冲区
+    buf_init(&txbuf, sizeof(arp_pkt_t));
+
+    //填写ARP报头
+    arp_pkt_t *arp_pkt = (arp_pkt_t *)txbuf.data;
+    *arp_pkt = arp_init_pkt;
+    memcpy(arp_pkt->target_ip, target_ip, NET_IP_LEN);
+
+    //设置操作类型
+    arp_pkt->opcode16 = swap16(ARP_REQUEST);
+
+    //发送ARP报文
+    ethernet_out(&txbuf, ether_broadcast_mac, NET_PROTOCOL_ARP);
 }
 
 /**
@@ -68,6 +82,21 @@ void arp_req(uint8_t *target_ip) {
  */
 void arp_resp(uint8_t *target_ip, uint8_t *target_mac) {
     // TO-DO
+    // Step1: 初始化缓冲区
+    buf_init(&txbuf, sizeof(arp_pkt_t));
+
+    // Step2: 填写ARP报头首部
+    arp_pkt_t *arp_pkt = (arp_pkt_t *)txbuf.data;
+    *arp_pkt = arp_init_pkt;
+    memcpy(arp_pkt->target_ip, target_ip, NET_IP_LEN);
+    memcpy(arp_pkt->target_mac, target_mac, NET_MAC_LEN);
+
+    // Step3: 设置操作类型为ARP_REPLY
+    arp_pkt->opcode16 = swap16(ARP_REPLY);
+
+    // Step4: 发送ARP响应报文
+    ethernet_out(&txbuf, target_mac, NET_PROTOCOL_ARP);
+
 }
 
 /**
@@ -78,6 +107,30 @@ void arp_resp(uint8_t *target_ip, uint8_t *target_mac) {
  */
 void arp_in(buf_t *buf, uint8_t *src_mac) {
     // TO-DO
+    // Step1: 检查数据包长度
+    if (buf->len < sizeof(arp_pkt_t)) {
+        return; // 数据包不完整，直接返回，丢弃该数据包
+    } 
+    
+    // Step2: 报头检查
+    arp_pkt_t *arp_pkt = (arp_pkt_t *)buf->data;
+    if (swap16(arp_pkt->hw_type16) != ARP_HW_ETHER || swap16(arp_pkt->pro_type16) != NET_PROTOCOL_IP || arp_pkt->hw_len != NET_MAC_LEN || arp_pkt->pro_len != NET_IP_LEN) {
+        return; // 报头不合法，直接返回，丢弃该数据包
+    }
+
+    // Step3: 更新ARP表项
+    map_set(&arp_table, arp_pkt->sender_ip, arp_pkt->sender_mac);
+
+    // Step4: 查看缓存情况
+    buf_t *cached_buf = map_get(&arp_buf, arp_pkt->sender_ip);
+    if (cached_buf) {
+        ethernet_out(cached_buf, arp_pkt->sender_mac, NET_PROTOCOL_IP);
+        map_delete(&arp_buf, arp_pkt->sender_ip); 
+    } else {
+        if (swap16(arp_pkt->opcode16) == ARP_REQUEST && memcmp(arp_pkt->target_ip, net_if_ip, NET_IP_LEN) == 0) {
+            arp_resp(arp_pkt->sender_ip, arp_pkt->sender_mac);
+        }
+    }
 }
 
 /**
@@ -89,6 +142,18 @@ void arp_in(buf_t *buf, uint8_t *src_mac) {
  */
 void arp_out(buf_t *buf, uint8_t *ip) {
     // TO-DO
+    // Step1: 查找ARP表
+    uint8_t *mac = map_get(&arp_table, ip);
+
+    //Step2: 找到对应的mac地址
+    if (mac) {
+        ethernet_out(buf, mac, NET_PROTOCOL_IP);
+    } else {
+        if (!map_get(&arp_buf, ip)) {
+           map_set(&arp_buf, ip, buf);
+            arp_req(ip); 
+        }
+    }
 }
 
 /**
